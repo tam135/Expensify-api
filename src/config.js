d7ca8cc332f7773aad9e02d8c18916e688db751d
Tam Phan <tnphan0204@gmail.com>
2019-09-09 17:54:32 -0400

    fixed db url

--- src/config.js
+++ src/config.js
@@ -3,5 +3,5 @@ module.exports = {
   NODE_ENV: process.env.NODE_ENV || "development",
   DB_URL:
     process.env.DB_URL ||
-    `postgres://excvceomftmxcp:6cb7b97fcfec1e3ea31e6b8025bb5801cc8613d68acfd3611f68353c7336e48f@ec2-54-221-212-126.compute-1.amazonaws.com:5432/d3b6dtdee4qn1r?ssl=true`
+    `postgres://bwmhwbrvhmpivu:54ce9c5166e6e94a29f49ac7481cf1316c0d212cffef0d7d12f0b5773fd976bd@ec2-54-221-212-126.compute-1.amazonaws.com:5432/d333h3ape40f1g?ssl=true`
 };
