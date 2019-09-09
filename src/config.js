7e2f15934f4f82127a23fca244d11ab1d352d3f8
Tam Phan <tnphan0204@gmail.com>
2019-09-09 18:15:42 -0400

    redeploy

--- src/config.js
+++ src/config.js
@@ -1,7 +1,5 @@
 module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || "development",
-  DB_URL:
-    process.env.DB_URL ||
-    `postgres://puijhbwsareyeb:2c8b068bb4029575541a9eda2eae88e63f3a5fd38890e03c856865efe2b46d4f@ec2-54-227-251-33.compute-1.amazonaws.com:5432/d2e7uvag1t9dq5?ssl=true`
+  DB_URL: process.env.DATABASE_URL || "postgresql://dunder-mifflin@localhost/expensify"
 };
