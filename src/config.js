5bc5aad9aa03650241b47a2bc2791686348c004d
Tam Phan <tnphan0204@gmail.com>
2019-09-09 18:00:42 -0400

    redploy

--- src/config.js
+++ src/config.js
@@ -3,5 +3,5 @@ module.exports = {
   NODE_ENV: process.env.NODE_ENV || "development",
   DB_URL:
     process.env.DB_URL ||
-    `postgres://bwmhwbrvhmpivu:54ce9c5166e6e94a29f49ac7481cf1316c0d212cffef0d7d12f0b5773fd976bd@ec2-54-221-212-126.compute-1.amazonaws.com:5432/d333h3ape40f1g?ssl=true`
+    `postgres://puijhbwsareyeb:2c8b068bb4029575541a9eda2eae88e63f3a5fd38890e03c856865efe2b46d4f@ec2-54-227-251-33.compute-1.amazonaws.com:5432/d2e7uvag1t9dq5?ssl=true`
 };
