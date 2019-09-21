a455574ebd8df789daa3d2c58260b5c0851ede76
Tam Phan <tnphan0204@gmail.com>
2019-09-21 18:09:46 -0400

    Changed incorrect username or password error

--- test/auth-endpoints.spec.js
+++ test/auth-endpoints.spec.js
@@ -3,7 +3,7 @@ const jwt = require('jsonwebtoken')
 const app = require('../src/app')
 const helpers = require('./test-helpers')
 
-describe('Auth endpoints', function() {
+describe.only('Auth endpoints', function() {
     let db 
     
     const { testUsers } = helpers.makeExpensesFixtures()
@@ -56,7 +56,7 @@ describe('Auth endpoints', function() {
                 return supertest(app)
                     .post('/api/auth/login')
                     .send(userInvalidUser)
-                    .expect(400, { error: `Incorrect user_name or password` })
+                    .expect(400, { error: `Incorrect username or password` })
             })  
 
             it(`responds 400 'invalid user_name or password' when bad password`, () => {
@@ -64,7 +64,7 @@ describe('Auth endpoints', function() {
                 return supertest(app)
                     .post('/api/auth/login')
                     .send(userInvalidPass)
-                    .expect(400, { error: `Incorrect user_name or password` })
+                    .expect(400, { error: `Incorrect username or password` })
             }) 
 
             it(`responds 200 and JWT auth token using secret when valid credentials`, () => {
